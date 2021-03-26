97b98ad0af920173e56445dd9e338b439f4d2c94
ARTUGIN_MIHAIL <mehailpost@gmail.com>
2021-03-26 21:53:20 +0300

    Refactoring SecurityUser.java

--- src/main/java/com/senla/srs/security/SecurityUser.java
+++ src/main/java/com/senla/srs/security/SecurityUser.java
@@ -1,8 +1,10 @@
 package com.senla.srs.security;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import com.senla.srs.model.UserStatus;
 import com.senla.srs.model.User;
+import lombok.NonNull;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,32 +13,19 @@ import java.util.Collection;
 import java.util.List;
 
 @Data
+@AllArgsConstructor
 public class SecurityUser implements UserDetails {
+    @NonNull
     private final String username;
+    @NonNull
     private final String password;
+    @NonNull
     private final List<SimpleGrantedAuthority> authorities;
     private final boolean isActive;
-
-    public SecurityUser(String username, String password, List<SimpleGrantedAuthority> authorities, boolean isActive) {
-        this.username = username;
-        this.password = password;
-        this.authorities = authorities;
-        this.isActive = isActive;
-    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return authorities;
-    }
-
-    @Override
-    public String getPassword() {
-        return password;
-    }
-
-    @Override
-    public String getUsername() {
-        return username;
     }
 
     @Override
@@ -68,5 +57,27 @@ public class SecurityUser implements UserDetails {
                 user.getStatus().equals(UserStatus.ACTIVE),
                 user.getRole().getAuthorities()
         );
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof SecurityUser)) return false;
+
+        SecurityUser that = (SecurityUser) o;
+
+        if (isActive() != that.isActive()) return false;
+        if (!getUsername().equals(that.getUsername())) return false;
+        if (!getPassword().equals(that.getPassword())) return false;
+        return getAuthorities().equals(that.getAuthorities());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getUsername().hashCode();
+        result = 31 * result + getPassword().hashCode();
+        result = 31 * result + getAuthorities().hashCode();
+        result = 31 * result + (isActive() ? 1 : 0);
+        return result;
     }
 }
