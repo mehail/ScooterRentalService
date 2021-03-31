49c987557b55f26beca1bd3e6d7c8fbfe52e9748
MIHAIL ARTUGIN <mehailpost@gmail.com>
2021-03-31 12:32:16 +0300

    Temp

--- src/main/java/com/senla/srs/dto/UserRequestDTO.java
+++ src/main/java/com/senla/srs/dto/UserRequestDTO.java
@@ -9,8 +9,6 @@ import lombok.NonNull;
 public class UserRequestDTO extends UserDTO{
     @NonNull
     private String password;
-    private String newPassword;
-    private String newEmail;
 
     public UserRequestDTO(@NonNull String email, @NonNull String firstName, @NonNull String lastName, @NonNull String password) {
         super(email, firstName, lastName);
--- src/main/java/com/senla/srs/mapper/UserRequestMapper.java
+++ src/main/java/com/senla/srs/mapper/UserRequestMapper.java
@@ -31,39 +31,23 @@ public class UserRequestMapper extends AbstractMapper<User, UserRequestDTO> {
         Optional<User> optionalUser = userService.retrieveUserByEmail(dto.getEmail());
         String cryptPassword = crypt(dto.getPassword());
 
-        if (!isExistUser(optionalUser)) {
+        if (optionalUser.isPresent()) {
+            user.setId(optionalUser.get().getId());
+        } else {
+            user.setId(null);
+        }
+
+        if (optionalUser.isEmpty()) {
 
             user.setPassword(cryptPassword);
             user.setBalance(0);
             user.setRole(Role.USER);
             user.setStatus(UserStatus.ACTIVE);
 
-        } else if (isMatchPassword(optionalUser, cryptPassword)){
-
-            User existUser = optionalUser.get();
-
-            user.setId(existUser.getId());
-
-            if (dto.getNewEmail() != null) {
-                user.setEmail(dto.getNewEmail());
-            }
-
-            if (dto.getNewPassword() != null) {
-                user.setEmail(dto.getNewPassword());
-            }
-
-            user.setFirstName(dto.getFirstName());
-            user.setLastName(dto.getLastName());
-            user.setRole(existUser.getRole());
-            user.setStatus(existUser.getStatus());
-            user.setBalance(existUser.getBalance());
+            //ToDo Если пользователь есть, по email
         }
 
         return user;
-    }
-
-    private boolean isExistUser(Optional<User> optionalUser) {
-        return optionalUser.isPresent();
     }
 
     private boolean isMatchPassword(Optional<User> optionalUser, String cryptPassword) {
