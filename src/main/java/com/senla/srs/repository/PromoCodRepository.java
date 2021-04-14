19ab699db17f9a3b64e19b62d72778e5209d2cee
ARTUGIN_MIHAIL <mehailpost@gmail.com>
2021-04-14 22:03:41 +0300

    Refactoring PromoCodRepository method

--- src/main/java/com/senla/srs/repository/PromoCodRepository.java
+++ src/main/java/com/senla/srs/repository/PromoCodRepository.java
@@ -4,6 +4,9 @@ import com.senla.srs.model.PromoCod;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PromoCodRepository extends JpaRepository<PromoCod, String> {
+    Optional<PromoCod> findByName(String name);
 }
