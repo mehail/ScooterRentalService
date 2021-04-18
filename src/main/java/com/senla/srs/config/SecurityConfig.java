4e4e4f26978e3568c16cbbdf7372f2400655c403
ARTUGIN_MIHAIL <mehailpost@gmail.com>
2021-04-18 15:07:49 +0300

    Replaced Swagger 2 with Swagger 3

--- src/main/java/com/senla/srs/config/SecurityConfig.java
+++ src/main/java/com/senla/srs/config/SecurityConfig.java
@@ -24,18 +24,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             "/api/v1/auth/login",
             "/api/v1/users",
 
-            // -- Swagger UI v2
-            "/v2/api-docs",
-            "/swagger-resources",
-            "/swagger-resources/**",
-            "/configuration/ui",
-            "/configuration/security",
-            "/swagger-ui.html",
-            "/webjars/**",
             // -- Swagger UI v3 (OpenAPI)
             "/v3/api-docs/**",
             "/swagger-ui/**"
-            // other public endpoints of your API may be appended to this array
     };
 
     public SecurityConfig(JwtConfig jwtConfig) {
