80e99f44a69cd97a0daf6102b69f94de31118afd
ARTUGIN_MIHAIL <mehailpost@gmail.com>
2021-04-27 15:16:39 +0300

    Refactoring UserRequestMapper

--- src/main/java/com/senla/srs/mapper/UserRequestMapper.java
+++ src/main/java/com/senla/srs/mapper/UserRequestMapper.java
@@ -2,35 +2,23 @@ package com.senla.srs.mapper;
 
 import com.senla.srs.dto.user.UserRequestDTO;
 import com.senla.srs.model.User;
-import com.senla.srs.service.UserService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
-import java.util.Optional;
-
 @Component
 public class UserRequestMapper extends AbstractMapper<User, UserRequestDTO> {
-    private final UserService userService;
     private final int encryptionStrength;
 
-    UserRequestMapper(UserService userService, @Value("${jwt.encryption.strength}") int encryptionStrength) {
+    UserRequestMapper(@Value("${jwt.encryption.strength}") int encryptionStrength) {
         super(User.class, UserRequestDTO.class);
-        this.userService = userService;
         this.encryptionStrength = encryptionStrength;
     }
 
     @Override
     public User toEntity(UserRequestDTO dto) {
-        User user = super.toEntity(dto);
-
-        Optional<User> optionalUser = userService.retrieveUserByEmail(dto.getEmail());
-
-        optionalUser.ifPresent(existUser -> user.setId(existUser.getId()));
-
-        user.setPassword(cryptPassword(dto.getPassword()));
-
-        return user;
+        dto.setPassword(cryptPassword(dto.getPassword()));
+        return super.toEntity(dto);
     }
 
     public String cryptPassword(String rowPassword) {
