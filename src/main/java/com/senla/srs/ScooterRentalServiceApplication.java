aae98f385c9e2085f241df8c6c5ffcfcc3fc2772
MIHAIL ARTUGIN <mehailpost@gmail.com>
2021-04-29 07:04:20 +0300

    Refactoring

--- src/main/java/com/senla/srs/ScooterRentalServiceApplication.java
+++ src/main/java/com/senla/srs/ScooterRentalServiceApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Starter {
+public class ScooterRentalServiceApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(Starter.class, args);
+		SpringApplication.run(ScooterRentalServiceApplication.class, args);
 	}
 }
