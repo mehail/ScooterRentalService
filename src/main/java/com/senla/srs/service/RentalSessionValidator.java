f6039441ccbafc43b910f4d159cbf22dbb2feb42
MIHAIL ARTUGIN <mehailpost@gmail.com>
2021-05-05 07:31:06 +0300

    Refactoring
    Refactoring

--- src/main/java/com/senla/srs/service/RentalSessionValidator.java
+++
@@ -1,7 +0,0 @@
-package com.senla.srs.service;
-
-import com.senla.srs.dto.rentalsession.RentalSessionRequestDTO;
-
-public interface RentalSessionValidator {
-    boolean isValid(RentalSessionRequestDTO rentalSessionRequestDTO);
-}
