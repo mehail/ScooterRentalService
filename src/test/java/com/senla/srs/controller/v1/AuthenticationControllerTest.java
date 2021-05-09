32d8c71d15ba23989f7e01e8120f4ce47981163e
ARTUGIN_MIHAIL <mehailpost@gmail.com>
2021-05-10 02:35:10 +0300

    Test6

--- src/test/java/com/senla/srs/controller/v1/AuthenticationControllerTest.java
+++ src/test/java/com/senla/srs/controller/v1/AuthenticationControllerTest.java
@@ -9,37 +9,30 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-//@SpringBootTest
+@SpringBootTest
 class AuthenticationControllerTest {
-//    @Value("${auth.login.admin}")
-//    private String adminLogin;
-//    @Value("${auth.password.admin}")
-//    private String adminPassword;
-//    @Autowired
-//    private AuthenticationController authenticationController;
+    @Value("${auth.login.admin}")
+    private String adminLogin;
+    @Value("${auth.password.admin}")
+    private String adminPassword;
+    @Autowired
+    private AuthenticationController authenticationController;
 
-//    @Test
-//    void createAuthenticationController() {
-//        assertThat(authenticationController).isNotNull();
-//    }
+    @Test
+    void authenticateOk() {
+        ResponseEntity<?> responseEntity =
+                authenticationController.authenticate(new AuthenticationRequestDTO(adminLogin, adminPassword));
 
-//    @Test
-//    void authenticateOk() {
-//        ResponseEntity<?> responseEntity =
-//                authenticationController.authenticate(new AuthenticationRequestDTO(adminLogin, adminPassword));
-//
-//        Assertions.assertEquals(HttpStatus.OK, responseEntity.getStatusCode(),
-//                "Authentication failed with valid login and password");
-//    }
-//
-//    @Test
-//    void authenticateForbidden() {
-//        ResponseEntity<?> responseEntity =
-//                authenticationController.authenticate(new AuthenticationRequestDTO(adminLogin, "otherPassword"));
-//
-//        Assertions.assertEquals(HttpStatus.FORBIDDEN, responseEntity.getStatusCode(),
-//                "Authentication completed successfully with invalid login password");
-//    }
+        Assertions.assertEquals(HttpStatus.OK, responseEntity.getStatusCode(),
+                "Authentication failed with valid login and password");
+    }
+
+    @Test
+    void authenticateForbidden() {
+        ResponseEntity<?> responseEntity =
+                authenticationController.authenticate(new AuthenticationRequestDTO(adminLogin, "otherPassword"));
+
+        Assertions.assertEquals(HttpStatus.FORBIDDEN, responseEntity.getStatusCode(),
+                "Authentication completed successfully with invalid login password");
+    }
 }
